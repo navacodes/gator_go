58889b750994ec60828b112d0163b764fe46e401
dnava24 <61266103+navacodes@users.noreply.github.com>
2025-09-03 22:44:55 -0500

    write function in config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -36,3 +36,32 @@ func Read() (Config, error) {
 
 	return config, nil
 }
+
+// SetUser updates the current username in the config and saves it back to the file
+
+func (c *Config) SetUser(username string) error {
+	// update the username field memory
+	c.CurrentUsername = username
+
+	// get home directory to write back to the file
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	// build the config path
+	configPath := filepath.Join(homeDir, ".gatorconfig.json")
+
+	//marshal(returns json as a byte slice) the updated config to json
+	data, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	//write the updated json data back to the file
+	// 0600 is the file permission(read and write for the owner only)
+	err = os.WriteFile(configPath, data, 0600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
